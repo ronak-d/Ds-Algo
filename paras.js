59cdded637ab13954b3079fc5ac91b4d2f80535e
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-04 23:46:36 +0530

    using stack

---
+++ paras.js
@@ -0,0 +1,48 @@
+// Ques = Check for the valid Paranthesis;
+
+const stack = [];
+
+// let qinput = '{([]{}())}';
+// let qinput = '{)';
+// let str = qinput.split("");
+// let input = str;
+// console.log(input);
+
+let input = ["{","{","(","[","]","{","}","(",")",")","}",] // output = false extra { ;
+
+const Para = () =>{
+
+    for(i=0; i<input.length; i++){
+
+        if(input[i] == '{' || input[i] == '[' || input[i] == '('){
+            stack.push(input[i]);
+        }
+        else if(input[i] == '}'){
+            stack[stack.length-1] == "{";
+            stack.pop();
+        }
+        else if(input[i] == ']'){
+            stack[stack.length-1] == "[";
+            stack.pop();
+        }
+        else if(input[i] == ')'){
+            stack[stack.length-1] == "(";
+            stack.pop();
+        }
+    }
+    if(stack.length !== 0){
+        console.log(stack)
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+let ques = Para();
+console.log(ques);
+
+
+
+// let OParas = ["{", "[", "("];
+// let CParas = ["}", "]", ")"];
