c26f45f8b012a7ac4461f44903082e4bc1d3c9d2
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-05 23:56:30 +0530

    boilerplate

--- reverse.js
+++ reverse.js
@@ -26,3 +26,27 @@
 // Sample Output
 // 2 1 3 4
 // 30 20 10 40 50
+
+function runProgram(input){
+    console.log(input);
+}
+if (process.env.USER === "") {
+  runProgram(`hello`);
+} else {
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+    read += input;
+  });
+  process.stdin.on("end", function () {
+    read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
