47466cd7d1bdabf7a70c3745d19077bd3ab0d0a5
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-08 16:18:07 +0530

    bf

--- sumZero.js
+++ sumZero.js
@@ -1,6 +1,6 @@
 // Ques - Find the pair in an arr where output = 0;
 
-const arr = [-5,-4,-3,-4,4,-5,3];
+let arr = [-5,-4,-3,-4,4,-5,3];
 // how many pairs are there which will be paired causes 0;
 // maintain counter for that pair.
 
@@ -23,3 +23,6 @@ const findP = ()=>{
 
     return count;
 }
+
+let iszero = findP(arr);
+console.log(iszero);
