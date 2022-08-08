f7a11fb6f22cab5ea61710ba8d7042ded4102334
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-08 16:16:42 +0530

    brute force

--- sumZero.js
+++ sumZero.js
@@ -1,6 +1,25 @@
-// ques - Find the pair in an arr where output = 0;
+// Ques - Find the pair in an arr where output = 0;
 
 const arr = [-5,-4,-3,-4,4,-5,3];
 // how many pairs are there which will be paired causes 0;
 // maintain counter for that pair.
 
+// approach - Brute force
+
+const findP = ()=>{
+    let count = 0;
+
+    for(let i=0; i<arr.length; i++){
+        for(let j=i+1; j<arr.length; j++){
+
+            if(arr[i] - arr[j] == 0){
+                count++;
+            }
+            else{
+                continue;
+            }
+        }
+    }
+
+    return count;
+}
