33de0f3519e510cfae92edc252408193450da956
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-18 00:13:25 +0530

    usin pointers

--- sumZeroOptimized.js
+++ sumZeroOptimized.js
@@ -1,5 +1,27 @@
 // Ques - Find the pair in an arr where output = 0;
 
 let arr = [-5,-4,-3,-4,4,-5,3];
+
 // how many pairs are there which will be paired causes 0;
 // maintain counter for that pair.
+
+function sumzero(arr) {
+
+    let left = 0;
+    let right = arr.length-1;
+    let counter = 0;
+
+    while(left < right){
+
+        let sumpair = arr[left]+arr[right];
+
+        if(sumpair === 0){
+            counter++;
+            return (arr[left],arr[right]);
+        }
+        else if(sumpair > 0){
+            
+        }
+    }
+
+}
