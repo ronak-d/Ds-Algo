0c63fc755b827f0d60693ca064913bab83d4a9a0
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-17 23:51:26 +0530

    optimzed

---
+++ sumZeroOptimized.js
@@ -0,0 +1,5 @@
+// Ques - Find the pair in an arr where output = 0;
+
+let arr = [-5,-4,-3,-4,4,-5,3];
+// how many pairs are there which will be paired causes 0;
+// maintain counter for that pair.
