124f3d40e7dd91bd218a30f69c00baf43d847c9b
ronak-d <97451974+ronak-d@users.noreply.github.com>
2022-08-20 15:27:04 +0530

    recursion

---
+++ factorial.js
@@ -0,0 +1,11 @@
+let number = 5;
+let findfactorial = isFactorialof(number);
+
+let isFactorialof = (number) =>{
+
+    if(number == 1){
+        return 1
+    }
+    let value = number * (number - 1);
+
+}
